fef5243e02f29edd7f06bb00390ee4928fb7ec6e
marleypm16 <yarlempm16@gmail.com>
2025-07-22 09:39:30 -0300

    initial commit

---
+++ src/main/java/catalogo/project/Servico/Catalogo/ServicoCatalogoApplication.java
@@ -0,0 +1,13 @@
+package catalogo.project.Servico.Catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoCatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/catalogo/project/Servico/Catalogo/ServicoCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package catalogo.project.Servico.Catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
