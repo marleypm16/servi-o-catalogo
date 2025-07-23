c0a2e9d87165de90580364bd921655c620ffb892
marleypm16 <yarlempm16@gmail.com>
2025-07-23 11:04:57 -0300

    adicionando filtro de roles

--- src/main/java/catalogo/project/Servico/Catalogo/Config/SecurityConfig.java
+++ src/main/java/catalogo/project/Servico/Catalogo/Config/SecurityConfig.java
@@ -1,31 +1,50 @@
 package catalogo.project.Servico.Catalogo.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // 1. DESABILITA a proteção CSRF (A SOLUÇÃO PARA O SEU PROBLEMA)
+        return http
                 .csrf(csrf -> csrf.disable())
-
-                // 2. DEFINE as regras de autorização para os endpoints
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 1. Torna a sessão stateless
                 .authorizeHttpRequests(auth -> auth
-                        // Permite que qualquer requisição seja feita, desde que o usuário esteja autenticado
-                        .anyRequest().authenticated()
-                )
-
-                // 3. HABILITA a autenticação básica (HTTP Basic)
-                .httpBasic(withDefaults());
-
-        return http.build();
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll() // 2. Permite acesso ao endpoint de login
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll() // Se tiver um endpoint de registro
+                        .requestMatchers(HttpMethod.GET, "/produto").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/produto/:id").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.POST, "/produto").hasRole("ADMIN")
+                        .anyRequest().authenticated() // 3. Todas as outras requisições precisam de autenticação
+                ).exceptionHandling(exception -> exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) // 4. Adiciona seu filtro antes do filtro padrão
+                .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
